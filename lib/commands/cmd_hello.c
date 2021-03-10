f6a245d702645cffc802887509498e575d8f7db6
Stephan Linz <linz@li-pro.net>
2021-03-10 08:18:39 +0100

    new shell command 'hello' as library
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ lib/commands/cmd_hello.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019-2021 Li-Pro.Net
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <sys/printk.h>
+#include <shell/shell.h>
+
+static int cmd_hello(const struct shell *shell, size_t argc, char** argv)
+{
+        ARG_UNUSED(argc);
+        ARG_UNUSED(argv);
+
+        shell_print(shell, "Hello from shell.");
+
+	return 0;
+}
+
+SHELL_CMD_ARG_REGISTER(hello, NULL, "say hello", cmd_hello, 1, 0);
