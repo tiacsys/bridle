0b8d3456ac4fba6f5050f5f564934ef30abe0fd8
Stephan Linz <linz@li-pro.net>
2021-06-13 10:26:18 +0200

    Bridle API provides BRIDLEVERSION
    issues: #30
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ include/bridle_includes.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 TiaC Systems
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ *
+ * @brief Header files included by bridle.h.
+ */
+
+#ifndef BRIDLE_INCLUDE_BRIDLE_INCLUDES_H_
+#define BRIDLE_INCLUDE_BRIDLE_INCLUDES_H_
+
+#include <stddef.h>
+#include <zephyr/types.h>
+#include <limits.h>
+#include <toolchain.h>
+#include <bridle_version.h>
+
+#endif /* BRIDLE_INCLUDE_BRIDLE_INCLUDES_H_ */
