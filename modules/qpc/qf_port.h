b6eb5ee83198c3ab20333063d93767f6aea87014
Tobias Kaestner <tobias.kaestner.jena@googlemail.com>
2022-03-15 21:35:26 +0100

    wip - prepare QV integration

--- modules/qpc/qf_port.h
+++ modules/qpc/qf_port.h
@@ -1,9 +1,18 @@
 #ifndef __QF_PORT
 #define __QF_PORT
 
-#include <stdbool.h>
-#include <stdint.h>
-
+#include <zephyr.h>
 
 #include "qep_port.h"
+
+#ifdef CONFIG_QPC_AO_SUPPORT
+
+#define QF_CRIT_ENTRY(dummy)    
+#define QF_CRIT_EXIT(dummy)
+
+#include "qv_port.h"
+#include "qf.h"
+
+
+#endif //CONFIG_QPC_AO_SUPPORT
 #endif
---
+++ modules/qpc/qv_port.h
@@ -0,0 +1,7 @@
+#ifndef __QV_PORT
+#define __QV_PORT
+
+
+#include "qv.h"
+
+#endif
