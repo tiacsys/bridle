453e39936d73b724cf74d1fad18379d3dda8450e
Stephan Linz <linz@li-pro.net>
2022-03-27 20:13:28 +0200

    cmake: auto-load our new boilerplate by Zephyr
    Our new boilerplate is automatically included through
    ZephyrBuildConfig.cmake, the Zephyr Build Configuration
    CMake package.
    
    This is an backporting from Bridle v3.0 to the v2.7 LTS release.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ cmake/boilerplate.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) 2021-2022 TiaC Systems
+# SPDX-License-Identifier: Apache-2.0
+
+# This boilerplate is automatically included through ZephyrBuildConfig.cmake,
+# found in ${BRIDLE_BASE}/share/zephyrbuild-package/cmake/ as 
+# ZephyrBuildConfig.cmake. For more information regarding the Zephyr Build
+# Configuration CMake package, please refer to:
+#
+#   https://bridle.tiac-systems.net/doc/latest/zephyr/guides/zephyr_cmake_package.html#zephyr-build-configuration-cmake-package
+
+include(${BRIDLE_BASE}/cmake/version.cmake)
