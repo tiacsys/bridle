f171da59224713fcbdf87711af840b8d131a552a
Stephan Linz <linz@li-pro.net>
2022-09-27 07:03:12 +0200

    cmake: provide Zephyr SDK version guard
    Bridle 3.0.x depends at most on Zephyr SDK 0.14.x.
    
    This version guard avoids that later by Zephyr a
    too recent Zephyr SDK version will be set. Often
    the code base of Bridle and Zephyr is incopatieble
    with the latest and greatest Zephyr SDK release.
    
    issues: #64
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- share/tsn-package/cmake/TsnConfig.cmake
+++ share/tsn-package/cmake/TsnConfig.cmake
@@ -9,6 +9,13 @@ set(BRIDLE_RELATIVE_DIR "../../..")
 # The use of get_filename_component ensures that the final path variable will not contain `../..`.
 get_filename_component(BRIDLE_BASE ${CMAKE_CURRENT_LIST_DIR}/${BRIDLE_RELATIVE_DIR} ABSOLUTE)
 
+file(STRINGS ${BRIDLE_BASE}/scripts/tools-versions-minimum.txt
+     zephyr_sdk_string LIMIT_COUNT 1 REGEX "^zephyr-sdk="
+)
+
+string(REGEX MATCH "=([^ \t]*)" OUT_VAR "${zephyr_sdk_string}")
+set(BRIDLE_TOOLCHAIN_ZEPHYR_SDK_REQUIRED_VERSION ${CMAKE_MATCH_1})
+
 if(NOT NO_BOILERPLATE)
   include(${BRIDLE_BASE}/cmake/boilerplate.cmake NO_POLICY_SCOPE)
 endif()
