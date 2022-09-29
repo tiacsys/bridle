125e60bfb3df2b4c4b3fa79adb5d747fd4c3ec59
Stephan Linz <linz@li-pro.net>
2022-09-29 18:01:36 +0200

    cmake: expand Bridle versioning with build version
    Similar to the Zephyr versioning the result of 'git describe'
    will be injected as BRIDLE_BUILD_VERSION string at build time.
    
    issues: #59
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- cmake/gen_version_h.cmake
+++ cmake/gen_version_h.cmake
@@ -1,6 +1,32 @@
 # Copyright (c) 2021-2022 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
 
+cmake_minimum_required(VERSION 3.20.0)
+
+if(NOT DEFINED BRIDLE_BUILD_VERSION)
+  find_package(Git QUIET)
+  if(GIT_FOUND)
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
+      WORKING_DIRECTORY                ${BRIDLE_BASE}
+      OUTPUT_VARIABLE                  BRIDLE_BUILD_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_STRIP_TRAILING_WHITESPACE
+      ERROR_VARIABLE                   stderr
+      RESULT_VARIABLE                  return_code
+    )
+    if(return_code)
+      message(STATUS "git describe failed: ${stderr}")
+    elseif(NOT "${stderr}" STREQUAL "")
+      message(STATUS "git describe warned: ${stderr}")
+    endif()
+  endif()
+  if((DEFINED BRIDLE_BUILD_VERSION) AND
+     (NOT BRIDLE_BUILD_VERSION MATCHES "^bridle-"))
+    string(PREPEND BRIDLE_BUILD_VERSION "bridle-")
+  endif()
+endif()
+
 include(${BRIDLE_BASE}/cmake/modules/bridle/version.cmake)
 configure_file(${BRIDLE_BASE}/version.h.in ${OUT_DIR}/${OUT_FILE})
 file(APPEND ${OUT_DIR}/${EXT_FILE} "\n#include \"${OUT_FILE}\"\n")
