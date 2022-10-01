f9275f2c453f4fe5fc8385e6ea9925651acd3a65
Stephan Linz <linz@li-pro.net>
2022-10-01 09:21:52 +0200

    cmake: add new generic module to find Sphinx
    Bridle requires a minimal Sphinx version and specific
    tool set. This commit adds checks during the cmake
    invocation and before processing actual processing.
    
    issues: #64
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- cmake/modules/bridle/requirements.cmake
+++ cmake/modules/bridle/requirements.cmake
@@ -30,3 +30,35 @@ else()
       "Check: ${BRIDLE_BASE}/scripts/tools-versions-minimum.txt"
   )
 endif()
+
+# This will determine and set the required Sphinx version.
+file(STRINGS ${ZEPHYR_BASE}/scripts/requirements-doc.txt
+     sphinx_string LIMIT_COUNT 1 REGEX "^sphinx[~,<,>]?="
+)
+string(REGEX MATCH "=([^ \t]*)[^ \t]*,[<,>]([^ \t]*)"
+             OUT_VAR "${sphinx_string}"
+)
+if(NOT CMAKE_MATCH_1)
+  string(REGEX MATCH "=([^ \t]*)" OUT_VAR "${sphinx_string}")
+  if(CMAKE_MATCH_1)
+    set(ZEPHYR_SPHINX_REQUIRED_VERSION ${CMAKE_MATCH_1})
+  else()
+    message(FATAL_ERROR "Sphinx: malformatted PIP version string as input?\n"
+        "Got: '${sphinx_string}'\n"
+        "Check: ${ZEPHYR_BASE}/scripts/requirements-doc.txt"
+    )
+  endif()
+else()
+  if(CMAKE_MATCH_2)
+    if(CMAKE_MATCH_1 VERSION_LESS CMAKE_MATCH_2)
+      set(ZEPHYR_SPHINX_REQUIRED_VERSION ${CMAKE_MATCH_1})
+    else()
+      set(ZEPHYR_SPHINX_REQUIRED_VERSION ${CMAKE_MATCH_2})
+    endif()
+  else()
+    message(FATAL_ERROR "Sphinx: malformatted PIP version string as input?\n"
+        "Got: '${sphinx_string}'\n"
+        "Check: ${ZEPHYR_BASE}/scripts/requirements-doc.txt"
+    )
+  endif()
+endif()
