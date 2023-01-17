55e6f3d32046aa57edefd54a9d285514fdecb523
Andreas Kurz <andreas.kurz@methodpark.de>
2023-01-17 18:10:58 +0000

    Add devcontainer integration for vscode
    This will auto setup the whole build environment based on zephyr's docker
    containers. Prerequisites are:
    - docker is accessable by the user without using sudo
    - vscode with remote container extension is installed
    
    Signed-off-by: Andreas Kurz <andreas.kurz@methodpark.de>
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORKSPACE_DIR=$1
+REPO_NAME=$2
+
+cd $WORKSPACE_DIR
+
+if [ ! -d ".west" ]; then
+    west init -l $REPO_NAME
+fi
+
+west update
+west bridle-export
+
+
+pip3 install --upgrade --requirement zephyr/scripts/requirements.txt
+pip3 install --upgrade --requirement bridle/scripts/requirements.txt
---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WORKSPACE_DIR=$1
+
+cd $WORKSPACE_DIR
+
+west update
