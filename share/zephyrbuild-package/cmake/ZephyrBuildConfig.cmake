3cccb136618157f2d00995d9f3b8507145f9bf95
Stephan Linz <linz@li-pro.net>
2023-07-12 10:26:53 +0200

    cmake: add guard for Zephyr Build Configuration
    issues: #104
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- share/zephyrbuild-package/cmake/ZephyrBuildConfig.cmake
+++ share/zephyrbuild-package/cmake/ZephyrBuildConfig.cmake
@@ -1,5 +1,9 @@
-# Copyright (c) 2021-2022 TiaC Systems
+# Copyright (c) 2021-2023 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
+
+if(NOT DEFINED ZEPHYR_BASE)
+  message(FATAL_ERROR "ZEPHYR_BASE not set")
+endif()
 
 # The ZephyrBuildConfig is simply including Bridle package.
 include(${CMAKE_CURRENT_LIST_DIR}/../../bridle-package/cmake/BridleConfig.cmake)
