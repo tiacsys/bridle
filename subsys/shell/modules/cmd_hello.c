6196a9ea2c62041137b97ef882be1f141c4d64e9
Stephan Linz <linz@li-pro.net>
2024-01-02 12:13:57 +0100

    subsys: bring back command help messages
    When getopt() is active (CONFIG_SHELL_GETOPT=y), the Zephyr
    shell is not parsing command handler to print help message.
    It must be done explicitly inside the command implementation.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- subsys/shell/modules/cmd_hello.c
+++ subsys/shell/modules/cmd_hello.c
@@ -4,13 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/sys/printk.h>
 #include <zephyr/shell/shell.h>
 
 static int cmd_hello(const struct shell *shell, size_t argc, char** argv)
 {
         ARG_UNUSED(argc);
         ARG_UNUSED(argv);
+
+#ifdef CONFIG_SHELL_GETOPT
+	/* When getopt() is active, the shell sub-system is not
+	 * parsing command handler to print help message. It must
+	 * be done explicitly. But no need to use the getopt() or
+	 * getopt_long() for just one option. */
+	if (argv[1] && (!strcmp("-h", argv[1]) || !strcmp("--help", argv[1]))) {
+		shell_help(shell);
+		return SHELL_CMD_HELP_PRINTED;
+	} else if (argc > 1) {
+		shell_error(shell, "Invalid option or usage.");
+		return -EINVAL;
+	}
+#endif /* CONFIG_SHELL_GETOPT */
 
         shell_print(shell, "Hello from shell.");
 
