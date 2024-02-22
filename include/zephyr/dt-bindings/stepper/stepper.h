284f71a2afc63da1e007e36a6a0ae450c677246d
Jilay Pandya <jilay.pandya@zeiss.com>
2024-02-22 14:05:16 +0100

    [WIP] drivers: introduce a new Stepper Motor API
    WARNING: This is proof of concept!
    
    Original contributed by Carl Zeiss Meditec AG, Jilay Pandya
    as Zephyr PR to RFC "Introduce Stepper Motor Controller API":
    
    - https://github.com/zephyrproject-rtos/zephyr/issues -> 68547
    - https://github.com/zephyrproject-rtos/zephyr/pull   -> 68774
    
    This commit introduces a basic API for Stepper Motor drivers,
    a set of substantially necessary functions for the electrical
    driving of stepper motors via their more or less intelligent
    current drivers (H-bridges). This also includes FAULT handling,
    for which corresponding event handling and a callback system
    for the higher level APIs are still missing.
    
    Principle control tasks for stepper motors are only introduced
    with an additional API or subsystem. This API therefore does
    not support any positioning in an absolute reference system.
    Instead, the stepper motors run freely at a specified rotational
    speed in their own relative step position.
    
    From a control point of view, this API only emphasizes the proper
    handling of microsteps and their effect on the electrical system
    and the rotational speed, nothing more.
    
    Signed-off-by: Dipak Shetty <dipak.shetty@zeiss.com>
    Signed-off-by: Florian Guhl <florian.guhl@zeiss.com>
    Signed-off-by: Jilay Pandya <jilay.pandya@zeiss.com>
    Signed-off-by: Stephan Linz <linz@li-pro.net>
    Signed-off-by: Sarah Renkhoff <sarah.renkhoff@navimatix.de>

---
+++ include/zephyr/dt-bindings/stepper/stepper.h
@@ -0,0 +1,59 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 Carl Zeiss Meditec AG
+ * SPDX-FileCopyrightText: Copyright (c) 2024 TiaC Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_STEPPER_STEPPER_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_STEPPER_STEPPER_H_
+
+/**
+ * @brief Stepper Motor Interface
+ * @defgroup stepper_interface Stepper Motor Interface
+ * @ingroup io_interfaces
+ * @{
+ */
+
+/**
+ * @name Stepper motor microstep resolution flags.
+ *
+ * @details Stepper motors move in discrete steps, or fractions of a
+ * revolution. For example, a stepper motor with a 1.8° step angle will
+ * make 200 steps for every full revolution of the motor (360° / 1.8°).
+ * This discrete motion means the motor´s rotation isn't perfectly smooth,
+ * and the slower the rotation, the less smooth it is due to the relatively
+ * large step size. One way to alleviate this lack of smoothness at slow
+ * speeds is to reduce the size of the motor’s steps by microstepping.
+ * Microstepping control divides each full step into smaller steps to help
+ * smooth out the motor´s rotation. For example, a 1.8° step can be divided
+ * up to 256 times, providing a step angle of 0.007° (1.8° / 256), or
+ * 51,200 microsteps per revolution.
+ *
+ * @note: Not all stepper motor drivers allow microstepping. It is achieved
+ * by using Pulse-Width Modulated (PWM) voltage to control current to the
+ * motor windings. The driver sends two voltage sine waves, 90° out of phase,
+ * to the motor windings. While current increases in one winding, it decreases
+ * in the other winding. This gradual transfer of current results in smoother
+ * motion and more consistent torque production than full- or half-step control.
+ *
+ * @{
+ */
+
+#define STEPPER_USTEP_RES_1		(1U << 0) /**< step angle x 1. */
+#define STEPPER_USTEP_RES_2		(1U << 1) /**< step angle x 1/2. */
+#define STEPPER_USTEP_RES_4		(1U << 2) /**< step angle x 1/4. */
+#define STEPPER_USTEP_RES_8		(1U << 3) /**< step angle x 1/8. */
+#define STEPPER_USTEP_RES_16		(1U << 4) /**< step angle x 1/16. */
+#define STEPPER_USTEP_RES_32		(1U << 5) /**< step angle x 1/32. */
+#define STEPPER_USTEP_RES_64		(1U << 6) /**< step angle x 1/64. */
+#define STEPPER_USTEP_RES_128		(1U << 7) /**< step angle x 1/128. */
+#define STEPPER_USTEP_RES_256		(1U << 8) /**< step angle x 1/256. */
+
+/** @} */
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_STEPPER_STEPPER_H_ */
