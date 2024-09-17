6d40e42dfd6e051b57cc0fe38344a74230fc6107
Sarah Renkhoff <sarah.renkhoff@navimatix.com>
2024-09-17 16:32:31 +0200

    [WIP] drivers: Create PWM-less move implementation for DRV84xx

--- drivers/stepper/stepper_drv84xx.c
+++ drivers/stepper/stepper_drv84xx.c
@@ -9,9 +9,18 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/stepper.h>
 #include <zephyr/logging/log.h>
+
+#include "zephyr/kernel.h"
 LOG_MODULE_REGISTER(stepper_drv84xx, CONFIG_STEPPER_LOG_LEVEL);
 
 #define DT_DRV_COMPAT drv84xx
+
+enum drv84xx_stepping_mode {
+	DRV84XX_STEPPING_MODE_OFF,
+	DRV84XX_STEPPING_MODE_POSITIONING,
+	DRV84XX_STEPPING_MODE_VELOCITY,
+	DRV84XX_STEPPING_MODE_ACCELERATION
+};
 
 struct drv84xx_config {
 	struct gpio_dt_spec dir_pin;
@@ -22,7 +31,84 @@ struct drv84xx_config {
 	struct gpio_dt_spec m1_pin;
 };
 
-struct drv84xx_data {};
+struct drv84xx_data {
+	/** Stepping speed used in positioning mode (steps/s) */
+	uint32_t positioning_speed;
+	/** Stepper mode */
+	atomic_t stepper_mode;
+	/** Remaining stepps in positioning mode */
+	atomic_t remaining_steps;
+	/** Positioning mode speed in steps/s */
+	atomic_t stepper_velocity;
+	/** Stack area used by stepper thread */
+	k_thread_stack_t *stepper_thread_stack;
+	/** Stepper thread data */
+	struct k_thread stepper_thread_data;
+	/** PID of stepper thread */
+	k_tid_t stepper_thread_pid;
+};
+
+static void gpio_stepper_thread_fn(void *dev_raw, void *arg2, void *arg3) {
+	const struct device *dev = (struct device *)dev_raw;
+	const struct drv84xx_config *const config = dev->config;
+	struct drv84xx_data *data = dev->data;
+	k_timeout_t pulse_time = K_FOREVER;
+	bool rising = true;
+
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+
+	/* Main thread loop */
+	while (true) {
+		/* Get mode */
+		atomic_t mode = atomic_get(&data->stepper_mode);
+
+		if (mode == DRV84XX_STEPPING_MODE_OFF) {
+			/* If off, do nothing and check again in 1ms */
+			pulse_time = K_MSEC(1); /* 1/ms */
+
+		} else if (mode == DRV84XX_STEPPING_MODE_POSITIONING) {
+			/* In positioning mode, alternate between setting pin
+			 * high and low */
+			gpio_pin_set_dt(&config->step_pin, rising);
+
+			if (!rising) {
+				/* Decrement counter on falling edges */
+				atomic_dec(&data->remaining_steps);
+			}
+			rising = !rising;
+
+			/* If we're done, set mode to off for next iteration */
+			if (atomic_get(&data->remaining_steps) == 0) {
+				atomic_set(&data->stepper_mode,
+					   DRV84XX_STEPPING_MODE_OFF);
+			}
+
+			pulse_time = K_NSEC(
+			    (NSEC_PER_SEC / data->positioning_speed) / 2);
+
+		} else if (mode == DRV84XX_STEPPING_MODE_VELOCITY) {
+			/* In velocity mode, alternate between setting pin high
+			 * and low */
+			gpio_pin_set_dt(&config->step_pin, rising);
+			rising = !rising;
+
+			pulse_time =
+			    K_NSEC((NSEC_PER_SEC /
+				    atomic_get(&data->stepper_velocity)) /
+				   2);
+
+		} else {
+			LOG_ERR(
+			    "Invalid mode encountered in "
+			    "gpio_stepper_thread_fn: %ld",
+			    mode);
+			return;
+		}
+
+		k_sleep(pulse_time);
+	}
+}
 
 static int drv84xx_on(const struct device *dev, const uint8_t motor) {
 	int ret;
@@ -107,6 +193,7 @@ static const struct stepper_api drv84xx_api = {
 
 static int drv84xx_init(const struct device *dev) {
 	const struct drv84xx_config *const config = dev->config;
+	struct drv84xx_data *data = dev->data;
 	int ret = 0;
 
 	/* Configure direction pin */
@@ -163,6 +250,16 @@ static int drv84xx_init(const struct device *dev) {
 		return ret;
 	}
 
+	/* Create stepper thread */
+	size_t stepper_thread_stack_size = CONFIG_DRV84XX_THREAD_STACK_SIZE;
+	data->stepper_thread_stack =
+	    k_thread_stack_alloc(stepper_thread_stack_size, 0);
+	data->stepper_thread_pid = k_thread_create(
+	    &data->stepper_thread_data, data->stepper_thread_stack,
+	    K_THREAD_STACK_SIZEOF(*data->stepper_thread_stack),
+	    gpio_stepper_thread_fn, (void *)dev, NULL, NULL,
+	    CONFIG_DRV84XX_THREAD_PRIORITY, 0, K_NO_WAIT);
+
 	return 0;
 }
 
@@ -175,7 +272,10 @@ static int drv84xx_init(const struct device *dev) {
 	    .m0_pin = GPIO_DT_SPEC_INST_GET_OR(inst, m0_gpios, {0}),       \
 	    .m1_pin = GPIO_DT_SPEC_INST_GET_OR(inst, m1_gpios, {0}),       \
 	};                                                                 \
-	static struct drv84xx_data drv84xx_data_##inst = {};               \
+	static struct drv84xx_data drv84xx_data_##inst = {                 \
+	    .positioning_speed = 100,                                      \
+	    .stepper_mode = DRV84XX_STEPPING_MODE_OFF,                     \
+	};                                                                 \
 	STEPPER_DEVICE_DT_INST_DEFINE(                                     \
 	    inst, &drv84xx_init,	  /* Init */                       \
 	    NULL,			  /* PM */                         \
