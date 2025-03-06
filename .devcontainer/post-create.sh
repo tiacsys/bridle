cda7179866789170d18027f966a8e7df528cf582
Andreas Kurz <info@akurz.net>
2025-03-06 18:41:10 +0000

    feat: use volume to store upstream modules
    This will add named volume to store all upstream modules. As volumes are
    owned by the root user the post-create script is adjusted to change
    permissions to the devcontainer's user.
    
    Signed-off-by: Andreas Kurz <info@akurz.net>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,15 +5,20 @@ REPO_NAME=$2
 
 cd $WORKSPACE_DIR
 
+# Adjust permissions as docker volumes is owned by root
+sudo chown -R $(id -g):$(id -u) $WORKSPACE_DIR
+
+# Init west workspace if not present
 if [ ! -d ".west" ]; then
     west init -l $REPO_NAME
 fi
 
+# Put .clangd file in expected location by clangd
 ln -s $REPO_NAME/.devcontainer/.clangd
 
+# Fetch upstream modules and setup tools
 west update
 west bridle-export
-
 
 pip3 install --upgrade --requirement zephyr/scripts/requirements.txt
 pip3 install --upgrade --requirement $REPO_NAME/scripts/requirements.txt
