af6de4134ec3bbcb4f21a2ef41af6aa88a10c1a2
Stephan Linz <linz@li-pro.net>
2025-05-24 12:59:24 +0200

    scripts: apply ruff lint rules
    This makes all Python scripts in scripts/ compliant w.r.t
    current Ruff rules.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- scripts/west_commands/zephyr_trampoline.py
+++ scripts/west_commands/zephyr_trampoline.py
@@ -1,24 +1,22 @@
 # Copyright (c) 2022 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
 
-from pathlib import Path
 import sys
-import os
-
 from functools import wraps
+from pathlib import Path
+
 from west import log
 
 
-def _get_zephyr(topdir, manifest):
-
+def _get_zephyr(topdir, manifest) -> str | None:
     ident = 'zephyr'
     projects = None
 
     try:
-        projects = manifest.get_projects([ident], allow_paths = False)
+        projects = manifest.get_projects([ident], allow_paths=False)
     except ValueError:
         try:
-            folder = os.path.join(Path(topdir), ident)
+            folder = Path(topdir) / ident
             projects = manifest.get_projects([folder])
         except ValueError:
             pass
@@ -28,44 +26,45 @@ def _get_zephyr(topdir, manifest):
     else:
         raise ValueError('Zephyr project not found.')
 
-def _get_west_cmd_path(project):
 
+def _get_west_cmd_path(project) -> Path | None:
     abspath = None
     cmdpath = None
 
     try:
-        abspath = project.abspath
-        cmdpath = project.userdata['west-commands-path']
+        abspath = Path(project.abspath)
+        cmdpath = Path(project.userdata['west-commands-path'])
     except (AttributeError, KeyError):
         pass
 
-    if abspath and cmdpath:
-        return os.path.join(abspath, cmdpath)
+    if abspath.exists() and str(cmdpath):
+        return (abspath / cmdpath).resolve()
     else:
-        raise ValueError('West command path not found.')
+        raise ValueError(f'West command path in project "{project}" not found.')
+
 
 def zephyr_sys_path(function):
-
     @wraps(function)
     def wrapper(self, *args, **kwargs):
-
         if 'sys_path_with_zephyr' not in function.__globals__:
-
             try:
                 zephyr = _get_zephyr(self.topdir, self.manifest)
             except ValueError:
-                log.err('Cannot find Zephyr project metadata.',
-                        'Check your west workspace or manifest.')
+                log.err(
+                    'Cannot find Zephyr project metadata.', 'Check your west workspace or manifest.'
+                )
                 return
 
             try:
                 west_cmd_path = _get_west_cmd_path(zephyr)
             except ValueError:
-                log.err('Cannot find West command path for Zephyr.',
-                        'Check your west workspace or manifest entries.')
+                log.err(
+                    'Cannot find West command path for Zephyr.',
+                    'Check your west workspace or manifest entries.',
+                )
                 return
 
-            sys.path.append(west_cmd_path)
+            sys.path.append(str(west_cmd_path))
             function.__globals__['sys_path_with_zephyr'] = sys.path
 
         retval = function(self, *args, **kwargs)
@@ -73,14 +72,14 @@ def zephyr_sys_path(function):
 
     return wrapper
 
-def zcmake_run_cmake(function):
 
+def zcmake_run_cmake(function):
     @zephyr_sys_path
     @wraps(function)
     def wrapper(self, *args, **kwargs):
-
         if 'run_cmake' not in function.__globals__:
             from zcmake import run_cmake
+
             function.__globals__['run_cmake'] = run_cmake
 
         retval = function(self, *args, **kwargs)
