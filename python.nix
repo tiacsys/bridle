40b073291dd330d562959b3bb9855fac6178dc96
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-06-13 13:56:44 +0200

    flake: Remove obsolete workaround
    The issue has since been fixed upstream.
    
    Signed-off-by: Sarah Renkhoff <sarah.renkhoff@navimatix.de>

--- python.nix
+++ python.nix
@@ -45,11 +45,6 @@ let
         sphinx-lint
         sphobjinv
         ;
-
-      # WORKAROUND for missing defusedxml in nativeCheckInputs of sphinx-sitemap derivation.
-      sphinx-sitemap = prev.sphinx-sitemap.overrideAttrs (old: {
-        pytestCheckPhase = ''true'';
-      });
     };
   };
 
