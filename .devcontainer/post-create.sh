c04bbce3352cdec43e9be5529acee3fe15587de3
Andreas Kurz <info@akurz.net>
2025-08-04 15:04:53 +0200

    chore[devcontainer]: minor fixups
    The clangd path changed due to newer container version. Also the
    clang-format file from the zephyr project is used for formatting
    code.
    
    Furthermore python packages are now installed in an virtual env
    which is automatically activated when a terminal is started.
    
    Signed-off-by: Andreas Kurz <info@akurz.net>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,12 +13,18 @@ if [ ! -d ".west" ]; then
     west init -l $REPO_NAME
 fi
 
-# Put .clangd file in expected location by clangd
-ln -s $REPO_NAME/.devcontainer/.clangd
-
 # Fetch upstream modules and setup tools
 west update
 west bridle-export
+
+# Put .clang files in expected location by clangd
+ln -s $REPO_NAME/.devcontainer/.clangd
+ln -s zephyr/.clang-format
+
+# Python Setup
+python3 -mvenv .venv
+
+. .venv/bin/activate
 
 pip3 install --upgrade --requirement zephyr/scripts/requirements.txt
 pip3 install --upgrade --requirement $REPO_NAME/scripts/requirements.txt
