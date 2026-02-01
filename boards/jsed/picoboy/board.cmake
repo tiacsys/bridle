2786e249b7659e56f78a0b6a393d11b000b973bb
Stephan Linz <linz@li-pro.net>
2026-02-01 08:03:30 +0100

    boards: picoboy: fix argument setup for UF2 runner
    This is an alignment to Zephyr upstream commit a0983a2f9192.
    
    The value of board qualifiers in CMake and Kconfig weere differ in the
    past. CMake has a leading '/', Kconfig not, as example:
    CMake:   BOARD_QUALIFIERS=/rp2040
    Kconfig: BOARD_QUALIFIERS=rp2040
    
    This was also discussed in Zephyr upstream PR 69740.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- boards/jsed/picoboy/board.cmake
+++ boards/jsed/picoboy/board.cmake
@@ -1,9 +1,9 @@
 # Copyright (c) 2023-2025 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
 
-if("${NORMALIZED_BOARD_QUALIFIERS}" STREQUAL "_rp2040")
+if("${NORMALIZED_BOARD_QUALIFIERS}" STREQUAL "rp2040")
   board_runner_args(uf2 "--board-id=RPI-RP2")
-elseif("${NORMALIZED_BOARD_QUALIFIERS}" STREQUAL "_rp2350a_m33")
+elseif("${NORMALIZED_BOARD_QUALIFIERS}" STREQUAL "rp2350a_m33")
   board_runner_args(uf2 "--board-id=RP2350")
 else()
   message(FATAL_ERROR "Unsupported board qualifiers: "
