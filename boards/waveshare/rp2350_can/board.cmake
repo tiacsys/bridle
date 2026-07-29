ae72b0c431d035955d7aa85ac9ff4b564cc9efc6
Stephan Linz <linz@li-pro.net>
2026-07-29 14:48:53 +0200

    boards: waveshare_rp2350_can: dts: convert to single board folder
    This introduces a separate directory for the Waveshare RP2350-CAN
    board in order to simplify the DTS, improve clarity, and better align
    with the conventions of Zephyr upstream. This is the first step toward
    potentially moving the board to Zephyr upstream or, alternatively,
    creating a bridle board extension here.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- boards/waveshare/rp2350_can/board.cmake
+++ boards/waveshare/rp2350_can/board.cmake
@@ -1,10 +1,10 @@
-# Copyright (c) 2025-2026 TiaC Systems
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 TiaC Systems
 # SPDX-License-Identifier: Apache-2.0
 
 message(STATUS "Found BOARD.cmake: ${dir}/board.cmake")
 
-# This configuration allows selecting what debug adapter debugging
-# waveshare_rp2350 by a command-line argument. It is mainly intended
+# This configuration allows selecting what debug adapter debugging the
+# waveshare_rp2350_can by a command-line argument. It is mainly intended
 # to support both the 'picoprobe' and 'raspberrypi-swd' adapter described
 # in "Getting started with Raspberry Pi Pico". Any other SWD debug adapter
 # might also be usable with this configuration.
@@ -12,7 +12,7 @@ message(STATUS "Found BOARD.cmake: ${dir}/board.cmake")
 # Set WAVESHARE_RP2350_DEBUG_ADAPTER to select debug adapter by command-line
 # arguments, e.g.:
 #
-#   west build -b <waveshare_rp2350_BOARD_VARIANT> \
+#   west build -b waveshare_rp2350_can \
 #              -- -DWAVESHARE_RP2350_DEBUG_ADAPTER=raspberrypi-swd
 #
 # The value is treated as a part of an interface file name contained in the
@@ -24,18 +24,26 @@ if ("${WAVESHARE_RP2350_DEBUG_ADAPTER}" STREQUAL "")
 	set(WAVESHARE_RP2350_DEBUG_ADAPTER "cmsis-dap")
 endif()
 
-board_runner_args(openocd --cmd-pre-init "source [find interface/${WAVESHARE_RP2040_DEBUG_ADAPTER}.cfg]")
+board_runner_args(openocd --cmd-pre-init "source [find interface/${WAVESHARE_RP2350_DEBUG_ADAPTER}.cfg]")
 board_runner_args(openocd --cmd-pre-init "transport select swd")
-board_runner_args(openocd --cmd-pre-init "source [find target/rp2350.cfg]")
+if(CONFIG_ARM)
+  board_runner_args(openocd --cmd-pre-init "source [find target/rp2350.cfg]")
+else()
+  board_runner_args(openocd --cmd-pre-init "source [find target/rp2350-riscv.cfg]")
+endif()
 
 # The adapter speed is expected to be set by interface configuration.
-# But if not so, set 2000 to adapter speed.
+# But if not so, set 2000 to adapter speed. The Raspberry Pi's OpenOCD
+# fork doesn't, so match their documentation at:
+# https://www.raspberrypi.com/documentation/microcontrollers/debug-probe.html#debugging-with-swd
 board_runner_args(openocd --cmd-pre-init "set_adapter_speed_if_not_set 2000")
 
+board_runner_args(probe-rs "--chip=RP235x")
 board_runner_args(jlink "--device=RP2350_M33_0")
 board_runner_args(uf2 "--board-id=RP2350")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 
