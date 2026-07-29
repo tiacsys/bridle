25f652a50e82ec3903ebd64e2d140244a67880e7
Stephan Linz <linz@li-pro.net>
2026-07-29 08:37:03 +0200

    include: zephyr: dt-bindings: gpio: add Waveshare RP2040 Mini defines
    To make the pin names of the Waveshare RP2040 Mini connector and
    solder pads easier to understand, we will introduce a macro definition
    for the pin names.
    
    And also add doxygen documentation for this connector.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ include/zephyr/dt-bindings/gpio/waveshare-rp2040mini-header.h
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Waveshare RP2040 Mini header pin constants
+ * @ingroup waveshare-rp2040mini-header
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_HEADER_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_HEADER_H_
+
+/**
+ * @defgroup waveshare-rp2040mini-header Waveshare RP2040 Mini header
+ * @brief Constants for pins exposed on Waveshare RP2040 Mini header
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define WS_RP2040MINI_HEADER_D0  0  /**< Digital pin 0 (GP0) */
+#define WS_RP2040MINI_HEADER_D1  1  /**< Digital pin 1 (GP1) */
+#define WS_RP2040MINI_HEADER_D2  2  /**< Digital pin 2 (GP2) */
+#define WS_RP2040MINI_HEADER_D3  3  /**< Digital pin 3 (GP3) */
+#define WS_RP2040MINI_HEADER_D4  4  /**< Digital pin 4 (GP4) */
+#define WS_RP2040MINI_HEADER_D5  5  /**< Digital pin 5 (GP5) */
+#define WS_RP2040MINI_HEADER_D6  6  /**< Digital pin 6 (GP6) */
+#define WS_RP2040MINI_HEADER_D7  7  /**< Digital pin 7 (GP7) */
+#define WS_RP2040MINI_HEADER_D8  8  /**< Digital pin 8 (GP8) */
+#define WS_RP2040MINI_HEADER_D9  9  /**< Digital pin 9 (GP9) */
+#define WS_RP2040MINI_HEADER_D10 10 /**< Digital pin 10 (GP10) */
+#define WS_RP2040MINI_HEADER_D11 11 /**< Digital pin 11 (GP11) */
+#define WS_RP2040MINI_HEADER_D12 12 /**< Digital pin 12 (GP12) */
+#define WS_RP2040MINI_HEADER_D13 13 /**< Digital pin 13 (GP13) */
+#define WS_RP2040MINI_HEADER_D14 14 /**< Digital pin 14 (GP14) */
+#define WS_RP2040MINI_HEADER_D15 15 /**< Digital pin 15 (GP15) */
+#define WS_RP2040MINI_HEADER_A0  16 /**< Analog pin 0 (ADC0/GP26) */
+#define WS_RP2040MINI_HEADER_A1  17 /**< Analog pin 1 (ADC1/GP27) */
+#define WS_RP2040MINI_HEADER_A2  18 /**< Analog pin 2 (ADC2/GP28) */
+#define WS_RP2040MINI_HEADER_A3  19 /**< Analog pin 3 (ADC2/GP29) */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_HEADER_H_ */
---
+++ include/zephyr/dt-bindings/gpio/waveshare-rp2040mini-pcbpads.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Waveshare RP2040 Mini solder pads pin constants
+ * @ingroup waveshare-rp2040mini-pcbpads
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_PCBPADS_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_PCBPADS_H_
+
+/**
+ * @defgroup waveshare-rp2040mini-pcbpads Waveshare RP2040 Mini solder pads
+ * @brief Constants for pins exposed on Waveshare RP2040 Mini solder pads
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define WS_RP2040MINI_PCBPAD_D0 0 /**< Digital pin 0 (GP17) */
+#define WS_RP2040MINI_PCBPAD_D1 1 /**< Digital pin 1 (GP18) */
+#define WS_RP2040MINI_PCBPAD_D2 2 /**< Digital pin 2 (GP19) */
+#define WS_RP2040MINI_PCBPAD_D3 3 /**< Digital pin 3 (GP20) */
+#define WS_RP2040MINI_PCBPAD_D4 4 /**< Digital pin 4 (GP21) */
+#define WS_RP2040MINI_PCBPAD_D5 5 /**< Digital pin 5 (GP22) */
+#define WS_RP2040MINI_PCBPAD_D6 6 /**< Digital pin 6 (GP23) */
+#define WS_RP2040MINI_PCBPAD_D7 7 /**< Digital pin 7 (GP24) */
+#define WS_RP2040MINI_PCBPAD_D8 8 /**< Digital pin 8 (GP25) */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2040MINI_PCBPADS_H_ */
