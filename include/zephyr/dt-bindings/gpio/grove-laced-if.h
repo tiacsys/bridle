b8dda05785d89dc7303cf589a0147b8ae9f9e4d6
Stephan Linz <linz@li-pro.net>
2026-07-29 08:37:03 +0200

    include: zephyr: dt-bindings: gpio: Add `seeed,grove-laced-if` defines
    To make the lane names of the Laced Grove signal interface easier to
    understand, we will introduce a macro definition for the lane names.
    
    And also add doxygen documentation for this connector.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ include/zephyr/dt-bindings/gpio/grove-laced-if.h
@@ -0,0 +1,57 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Laced Grove signal interface lane constants
+ * @ingroup grove-laced-if
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_GROVE_LACED_IF_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_GROVE_LACED_IF_H_
+
+/**
+ * @defgroup grove-laced-if Laced Grove signal interface
+ * @brief Constants for lanes exposed on laced Grove signal interface
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define GROVE_LIF_L0  0  /**< Lane 0: GPIO/PWM */
+#define GROVE_LIF_L1  1  /**< Lane 1: GPIO/PWM */
+#define GROVE_LIF_L2  2  /**< Lane 2: GPIO/PWM */
+#define GROVE_LIF_L3  3  /**< Lane 3: GPIO/PWM */
+#define GROVE_LIF_L4  4  /**< Lane 4: GPIO/PWM */
+#define GROVE_LIF_L5  5  /**< Lane 5: GPIO/PWM */
+#define GROVE_LIF_L6  6  /**< Lane 6: GPIO/PWM */
+#define GROVE_LIF_L7  7  /**< Lane 7: GPIO/PWM */
+#define GROVE_LIF_L8  8  /**< Lane 8: GPIO/PWM */
+#define GROVE_LIF_L9  9  /**< Lane 9: GPIO/PWM */
+#define GROVE_LIF_L10 10 /**< Lane 10: GPIO/PWM */
+#define GROVE_LIF_L11 11 /**< Lane 11: GPIO/PWM */
+#define GROVE_LIF_L12 12 /**< Lane 12: GPIO/PWM */
+#define GROVE_LIF_L13 13 /**< Lane 13: GPIO/PWM */
+#define GROVE_LIF_L14 14 /**< Lane 14: GPIO/PWM */
+#define GROVE_LIF_L15 15 /**< Lane 15: GPIO/PWM */
+#define GROVE_LIF_L16 16 /**< Lane 16: GPIO/PWM */
+#define GROVE_LIF_L17 17 /**< Lane 17: GPIO/PWM */
+#define GROVE_LIF_L18 18 /**< Lane 18: GPIO/PWM */
+#define GROVE_LIF_L19 19 /**< Lane 19: GPIO/PWM */
+#define GROVE_LIF_L20 20 /**< Lane 20: GPIO/PWM */
+#define GROVE_LIF_L21 21 /**< Lane 21: GPIO/PWM */
+#define GROVE_LIF_L22 22 /**< Lane 22: GPIO/PWM */
+#define GROVE_LIF_L23 23 /**< Lane 23: GPIO/PWM */
+#define GROVE_LIF_L24 24 /**< Lane 24: GPIO/PWM */
+#define GROVE_LIF_L25 25 /**< Lane 25: GPIO/PWM */
+#define GROVE_LIF_L26 26 /**< Lane 26: GPIO/PWM */
+#define GROVE_LIF_L27 27 /**< Lane 27: GPIO/PWM */
+#define GROVE_LIF_L28 28 /**< Lane 28: GPIO/PWM */
+#define GROVE_LIF_L29 29 /**< Lane 29: GPIO/PWM */
+#define GROVE_LIF_L30 30 /**< Lane 30: GPIO/PWM */
+#define GROVE_LIF_L31 31 /**< Lane 31: GPIO/PWM */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_GROVE_LACED_IF_H_ */
