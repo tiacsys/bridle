15c62550f3d6cb07b1622d0767f2eb82584a805b
Stephan Linz <linz@li-pro.net>
2026-07-29 08:37:03 +0200

    include: zephyr: dt-bindings: gpio: Add raspberrypi,pico-header-r3 defines
    To make the pin names of the Raspberry Pi Pico (R3) connector easier to
    understand, we will introduce a macro definition for the pin names.
    
    And also add doxygen documentation for this connector.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ include/zephyr/dt-bindings/gpio/raspberrypi-pico-header-r3.h
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Raspberry Pi Pico (R3) header pin constants
+ * @ingroup raspberrypi-pico-header-r3
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RASPBERRYPI_PICO_HEADER_R3_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RASPBERRYPI_PICO_HEADER_R3_H_
+
+/**
+ * @defgroup raspberrypi-pico-header-r3 Raspberry Pi Pico (R3) header
+ * @brief Constants for pins exposed on Raspberry Pi Pico (R3) header
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define RPIPICO_HEADER_R3_D0  0  /**< Digital pin 0 (GP0) */
+#define RPIPICO_HEADER_R3_D1  1  /**< Digital pin 1 (GP1) */
+#define RPIPICO_HEADER_R3_D2  2  /**< Digital pin 2 (GP2) */
+#define RPIPICO_HEADER_R3_D3  3  /**< Digital pin 3 (GP3) */
+#define RPIPICO_HEADER_R3_D4  4  /**< Digital pin 4 (GP4) */
+#define RPIPICO_HEADER_R3_D5  5  /**< Digital pin 5 (GP5) */
+#define RPIPICO_HEADER_R3_D6  6  /**< Digital pin 6 (GP6) */
+#define RPIPICO_HEADER_R3_D7  7  /**< Digital pin 7 (GP7) */
+#define RPIPICO_HEADER_R3_D8  8  /**< Digital pin 8 (GP8) */
+#define RPIPICO_HEADER_R3_D9  9  /**< Digital pin 9 (GP9) */
+#define RPIPICO_HEADER_R3_D10 10 /**< Digital pin 10 (GP10) */
+#define RPIPICO_HEADER_R3_D11 11 /**< Digital pin 11 (GP11) */
+#define RPIPICO_HEADER_R3_D12 12 /**< Digital pin 12 (GP12) */
+#define RPIPICO_HEADER_R3_D13 13 /**< Digital pin 13 (GP13) */
+#define RPIPICO_HEADER_R3_D14 14 /**< Digital pin 14 (GP14) */
+#define RPIPICO_HEADER_R3_D15 15 /**< Digital pin 15 (GP15) */
+#define RPIPICO_HEADER_R3_D16 16 /**< Digital pin 16 (GP16) */
+#define RPIPICO_HEADER_R3_D17 17 /**< Digital pin 17 (GP17) */
+#define RPIPICO_HEADER_R3_D18 18 /**< Digital pin 18 (GP18) */
+#define RPIPICO_HEADER_R3_D19 19 /**< Digital pin 19 (GP19) */
+#define RPIPICO_HEADER_R3_D20 20 /**< Digital pin 20 (GP20) */
+#define RPIPICO_HEADER_R3_D21 21 /**< Digital pin 21 (GP21) */
+#define RPIPICO_HEADER_R3_D22 22 /**< Digital pin 25 (GP22) */
+#define RPIPICO_HEADER_R3_A0  23 /**< Analog pin 0 (ADC0/GP26) */
+#define RPIPICO_HEADER_R3_A1  24 /**< Analog pin 1 (ADC1/GP27) */
+#define RPIPICO_HEADER_R3_A2  25 /**< Analog pin 2 (ADC2/GP28) */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_RASPBERRYPI_PICO_HEADER_R3_H_ */
