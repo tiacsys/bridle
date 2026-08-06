c9aaf89d18eb6c19a00f82f6f16e00298f95fe56
Stephan Linz <linz@li-pro.net>
2026-08-06 08:19:56 +0200

    dts: add Waveshare RP2350 Matrix layout bindings
    GPIO pins exposed on Waveshare RP2350 Matrix headers and solder pads.
    Also add related macro definition for the pin names on headers and
    solder pads together with the doxygen documentation.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ include/zephyr/dt-bindings/gpio/waveshare-rp2350matrix-header.h
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Waveshare RP2350 Matrix header pin constants
+ * @ingroup waveshare-rp2350matrix-header
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_HEADER_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_HEADER_H_
+
+/**
+ * @defgroup waveshare-rp2350matrix-header Waveshare RP2350 Matrix header
+ * @brief Constants for pins exposed on Waveshare RP2350 Matrix header
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define WS_RP2350MATRIX_HEADER_D0  0  /**< Digital pin 0 (GP10) */
+#define WS_RP2350MATRIX_HEADER_D1  1  /**< Digital pin 1 (GP11) */
+#define WS_RP2350MATRIX_HEADER_D2  2  /**< Digital pin 2 (GP12) */
+#define WS_RP2350MATRIX_HEADER_D3  3  /**< Digital pin 3 (GP13) */
+#define WS_RP2350MATRIX_HEADER_D4  4  /**< Digital pin 4 (GP14) */
+#define WS_RP2350MATRIX_HEADER_D5  5  /**< Digital pin 5 (GP15) */
+#define WS_RP2350MATRIX_HEADER_D6  6  /**< Digital pin 6 (GP16) */
+#define WS_RP2350MATRIX_HEADER_D7  7  /**< Digital pin 7 (GP17) */
+#define WS_RP2350MATRIX_HEADER_D8  8  /**< Digital pin 8 (GP18) */
+#define WS_RP2350MATRIX_HEADER_D9  9  /**< Digital pin 9 (GP19) */
+#define WS_RP2350MATRIX_HEADER_D10 10 /**< Digital pin 10 (GP22) */
+#define WS_RP2350MATRIX_HEADER_D11 11 /**< Digital pin 11 (GP23) */
+#define WS_RP2350MATRIX_HEADER_D12 12 /**< Digital pin 12 (GP24) */
+#define WS_RP2350MATRIX_HEADER_A0  13 /**< Analog pin 0 (ADC0/GP26) */
+#define WS_RP2350MATRIX_HEADER_A1  14 /**< Analog pin 1 (ADC1/GP27) */
+#define WS_RP2350MATRIX_HEADER_A2  15 /**< Analog pin 2 (ADC2/GP28) */
+#define WS_RP2350MATRIX_HEADER_A3  16 /**< Analog pin 3 (ADC2/GP29) */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_HEADER_H_ */
---
+++ include/zephyr/dt-bindings/gpio/waveshare-rp2350matrix-pcbpads.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Waveshare RP2350 Matrix solder pads pin constants
+ * @ingroup waveshare-rp2350matrix-pcbpads
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_PCBPADS_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_PCBPADS_H_
+
+/**
+ * @defgroup waveshare-rp2350matrix-pcbpads Waveshare RP2350 Matrix solder pads
+ * @brief Constants for pins exposed on Waveshare RP2350 Matrix solder pads
+ * @ingroup devicetree-gpio-pin-headers
+ * @{
+ */
+
+#define WS_RP2350MATRIX_PCBPAD_D0 0 /**< Digital pin 0 (GP0) */
+#define WS_RP2350MATRIX_PCBPAD_D1 1 /**< Digital pin 1 (GP1) */
+#define WS_RP2350MATRIX_PCBPAD_D2 2 /**< Digital pin 2 (GP2) */
+#define WS_RP2350MATRIX_PCBPAD_D3 3 /**< Digital pin 3 (GP3) */
+#define WS_RP2350MATRIX_PCBPAD_D4 4 /**< Digital pin 4 (GP4) */
+#define WS_RP2350MATRIX_PCBPAD_D5 5 /**< Digital pin 5 (GP5) */
+#define WS_RP2350MATRIX_PCBPAD_D6 6 /**< Digital pin 6 (GP20) */
+#define WS_RP2350MATRIX_PCBPAD_D7 7 /**< Digital pin 7 (GP21) */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_WAVESHARE_RP2350MATRIX_PCBPADS_H_ */
