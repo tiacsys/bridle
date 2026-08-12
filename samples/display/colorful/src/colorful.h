ec0891f52844c8da1e35e15ddf1fc7ec4b96ba42
Stephan Linz <linz@li-pro.net>
2026-08-12 20:09:18 +0200

    samples: display: add colorful RGB gradient sample
    Adds an alternative sample for using multiple display drivers with
    different pixel formats and color modells. It renders an colorful
    RGB gradient on standard LCD, OLED or LED (strip) matrix displays
    and grayscale gradient on monochrome displays or EPD (ePaper).
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ samples/display/colorful/src/colorful.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Based on Zephyr upstream display driver sample:
+ * Copyright (c) 2019 Jan Van Winkel <jan.van_winkel@dxplore.eu>
+ * Copyright (c) 2025 NXP
+ */
+
+#ifndef __SAMPLE_COLORFUL_H__
+#define __SAMPLE_COLORFUL_H__
+
+int sample_colorful_draw(void);
+
+#endif /* __SAMPLE_COLORFUL_H__ */
---
+++ samples/display/colorful/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 TiaC Systems
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Based on Zephyr upstream display driver sample:
+ * Copyright (c) 2019 Jan Van Winkel <jan.van_winkel@dxplore.eu>
+ * Copyright (c) 2025 NXP
+ */
+
+#include "colorful.h"
+
+int main(void)
+{
+	return sample_colorful_draw();
+}
