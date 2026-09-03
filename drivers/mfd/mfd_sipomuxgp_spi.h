a8ac1cc3335a41441851e007d0d0853f292e2665
Stephan Linz <linz@li-pro.net>
2026-09-03 17:50:27 +0200

    drivers: mfd: sipomuxgp: deprecate master/slave terminology with v5.0
    The SPI API has been migrated to the inclusive terminology.
    The former names like "master" or "slave" are deprecated and
    will be removed in Zephyr v5.0.
    
    SPI_OP_MODE_CONTROLLER and SPI_OP_MODE_PERIPHERAL replace
    SPI_OP_MODE_MASTER and SPI_OP_MODE_SLAVE.
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- drivers/mfd/mfd_sipomuxgp_spi.h
+++ drivers/mfd/mfd_sipomuxgp_spi.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2025 TiaC Systems
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026 TiaC Systems
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -21,8 +21,9 @@ typedef struct mfd_sipomuxgp_spi_config {
 
 #define MFD_SIPOMUXGP_spi_CFG_INIT(n, b)                                                           \
 	static const mfd_sipomuxgp_spi_config_t mfd_sipomuxgp_spi_config_##n = {                   \
-		.bus = SPI_DT_SPEC_GET(INST_DT_SIPOMUXGP(n, b),                                    \
-				       SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8)),   \
+		.bus = SPI_DT_SPEC_GET(INST_DT_SIPOMUXGP(n, b), SPI_OP_MODE_CONTROLLER |           \
+									SPI_TRANSFER_MSB |         \
+									SPI_WORD_SET(8)),          \
 	};
 
 int mfd_sipomuxgp_spi_init(const struct device *dev);
